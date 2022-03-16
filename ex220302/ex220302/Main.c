57e4fff6361ae91f4275f540eb8c614a31fb5559
GPlogger <kkjj4414@naver.com>
2022-03-16 23:47:43 +0900

    3.16 study

---
+++ ex220302/ex220302/Main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <iostream>
+int main() {
+	printf("Hello!\n");
+	printf("Hello!\n");
+	system("pause");
+}
---
+++ ex220316/ex220316/Main.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+int main()
+{
+	printf("Hello\n");
+	int n = 100;
+	double d = 5.5;
+	printf("%d\n", n);
+	printf("%g\n", d);
+}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("Hello");
+//	printf("%d", 100);
+//	printf("%g", 5.5);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("a b c d %d\n", 100);
+//	printf("%d %d\n", 100, 60);
+//	printf("%g %g\n", 5.55, 60.0);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("Hello!\n");
+//	printf("100\n");
+//	printf("5.5\n");
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("±èÁö¼º\n");
+//	getchar();
+//	printf("201810110\n");
+//	getchar();
+//	printf("3ÇÐ³â\n");
+//	getchar();
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("±èÁö¼º\n");
+//	printf("201810110\n");
+//	printf("3ÇÐ³â\n");
+//	getchar();
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("Hello!\n");
+//	getchar();
+//	printf("Hello!\n");
+//	getchar();
+//	printf("Hello!\n");
+//	getchar();
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("Hello!\n");
+//	printf("Hello!\n");
+//	printf("Hello!\n");
+//	printf("Hello!\n");
+//	printf("Hello!\n");
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("Hello!\n");
+//}
